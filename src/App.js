17353d38b977135d5b1a0ccfb3b0f498e4626c10
TechCare <narutominh747@gmail.com>
2022-02-20 01:11:05 +0700

    complete side-bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
+import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Sidebar from './Components/Sidebar/Sidebar';
+import { SidebarData } from './Components/Sidebar/SideBarData';
+
+function RenderComponent(props){
+  return (
+    <h4>{props.title}</h4>
+  )
+}
 
 function App() {
+  let SideBarDataButLast = SidebarData.slice(0, -1);
+
   return (
-    <div className="default-mode">
-      <Sidebar />
-    </div>
+    <Router>
+      <div className="default-mode">
+        <Sidebar />
+        <main className='main'>
+            <div className='main__curtain'></div>
+            <div className='main__content'>
+              <Routes>
+                {
+                  SideBarDataButLast && SideBarDataButLast.map((item, index) => <Route key={index} path={item.path} element={<RenderComponent title={item.title} />} />)
+                }
+                <Route path="/" element={<Navigate replace to="/" />} />
+              </Routes>
+            </div>
+        </main>
+      </div>
+    </Router>
   );
 }
 
--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,80 +1,79 @@
-import React, { Component, useState } from 'react'
+import React, { Component } from 'react'
 import * as FaIcons from "react-icons/fa";
-import * as AiIcons from "react-icons/ai";
 import './Sidebar.css';
+import { SidebarData } from './SideBarData';
+import { Link } from 'react-router-dom';
 
 export default class Sidebar extends Component {
   constructor(props){
     super(props);
+    
+    this.SidebarData = SidebarData;
+    this.SidebarDataLastItem = SidebarData[SidebarData.length - 1];
 
     this.state = {
-      toggle: false
+      sidebarToggle: false,
+      activeLink: this.SidebarData[0].title || null
     }
   }
 
-  toggleTrigger = (e) => {
-    this.setState({toggle: !this.state.toggle})
+  sidebarToggle = (e) => {
+    this.setState({sidebarToggle: !this.state.sidebarToggle})
   }; 
+
+  linkToggle = (title) => {
+    this.setState({activeLink: title});
+  }
   
-  setSidebarActive = () => this.state.toggle ? 'sidebar-active' : '';
+  setSidebarActive = () => this.state.sidebarToggle ? 'sidebar-active' : '';
   
+  setLinkActive = (title) => (this.state.activeLink === title) ? 'link-active' : '';
+
   render() {
-    let active = true;
     return (
       <>
-        <div className={`side-bar p-2 ${this.setSidebarActive()}`}>
-            <div className='side-bar__header m-2'>
-                <div className='side-bar__header-logo d-flex justify-content-start'>
+        <div className={`side-bar ${this.setSidebarActive()}`}>
+            <div className='side-bar__header'>
+                <div className='side-bar__header-logo'>
                   <FaIcons.FaMediumM className='side-bar__logo-icon' />
                 </div>
-                <i className='side-bar__toogle-button' onClick={this.toggleTrigger}>
+                <i className='side-bar__toogle-button' onClick={this.sidebarToggle}>
                   <FaIcons.FaBars />
                 </i>
             </div>
+
             <ul className='side-bar__nav-list'>
-                <li className='side-bar__nav-item link-active'>
-                    <a className='side-bar__nav-item-link d-flex align-items-center'>
-                        <i className='side-bar__nav-item-icon'>
-                          <FaIcons.FaToolbox />
-                        </i>
-                        <span className='side-bar__nav-item-name'>Projects</span>
-                    </a>
+                {
+                  SidebarData && SidebarData.slice(0, -1).map((item, index) => {
+                    return (
+                      <li className={`side-bar__nav-item ${this.setLinkActive(item.title)}`} key={index}>
+                          <Link to={item.path} className='side-bar__nav-item-link d-flex align-items-center' onClick={() => this.linkToggle(item.title)}>
+                              <i className='side-bar__nav-item-icon'>
+                                  {item.icon}
+                              </i>
+                              <span className='side-bar__nav-item-name'>{item.title}</span>
+                          </Link>
 
-                    <div className='side-bar__nav-item-tool-tip'>Projects</div>
-                </li>
-                <li className='side-bar__nav-item'>
-                    <a className='side-bar__nav-item-link d-flex align-items-center'>
-                        <i className='side-bar__nav-item-icon'>
-                          <AiIcons.AiFillSetting />
-                        </i>
-                        <span className='side-bar__nav-item-name'>Settings</span>
-                    </a>
-                    <span className='side-bar__nav-item-tool-tip'>Settings</span>
-                </li>
-                <li className='side-bar__nav-item'>
-                    <a className='side-bar__nav-item-link d-flex align-items-center'>
-                        <i className='side-bar__nav-item-icon'>
-                          <AiIcons.AiOutlineFileText/>
-                        </i>
-                        <span className='side-bar__nav-item-name'>Activities</span>
-                    </a>
+                          <div className='side-bar__nav-item-tool-tip'>{item.title}</div>
+                      </li>
+                    )
+                  })
+                }
 
-                    <span className='side-bar__nav-item-tool-tip'>Activities</span>
-                </li>
+                {
+                  SidebarData[SidebarData.length - 1] && 
+                   <li className='side-bar__nav-item side-bar__nav-item--footer'>
+                        <Link to={SidebarData[SidebarData.length - 1].path} className='side-bar__nav-item-link side-bar__nav-item-link--footer'>
+                            <i className='side-bar__nav-item-icon side-bar__nav-item-icon--footer'>
+                                {SidebarData[SidebarData.length - 1].icon}
+                            </i>
+                            <span className='side-bar__nav-item-name side-bar__nav-item-name--footer'>{SidebarData[SidebarData.length - 1].title}</span>
+                        </Link>
+
+                        <span className='side-bar__nav-item-tool-tip'>{SidebarData[SidebarData.length - 1].title}</span>
+                    </li>
+                }
             </ul>
-
-            <div className='side-bar__footer'>
-                <div className='side-bar__nav-item'>
-                    <a className='side-bar__nav-item-link d-flex align-items-center'>
-                        <i className='side-bar__nav-item-icon'>
-                            <FaIcons.FaSignOutAlt />
-                        </i>
-                        <span className='side-bar__nav-item-name'>Sign out</span>
-                    </a>
-
-                    <span className='side-bar__nav-item-tool-tip'>Sign out</span>
-                </div>
-            </div>
         </div>
       </>
     )
